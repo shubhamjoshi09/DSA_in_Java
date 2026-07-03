5a93baad94495370ad029f451faf28a38ec17fbd
Shubham Joshi <154412135+shubhamjoshi09@users.noreply.github.com>
2026-07-03 11:43:54 +0530

    2nd file

---
+++ Sorting/Slectionsort.java
@@ -0,0 +1,25 @@
+class Slectionsort{
+
+    public static void printarray (int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+       int arr[] = {6,3,5,2,4,1,7};
+       for(int i=0; i<arr.length; i++){
+           int smallest = i;
+           for(int j=i+1; j<arr.length; j++){
+             if(arr[smallest]> arr[j]){
+                smallest=j;
+             }
+           }
+           int temp = arr[smallest];
+           arr[smallest] = arr[i];
+           arr[i]= temp;
+       }
+
+    printarray(arr);
+    }
+}
