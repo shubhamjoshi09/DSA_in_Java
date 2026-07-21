1b6763a94288d196fe3a3533a6f5e7bdab2761f0
lucky <sj476267@gmail.com>
2026-07-21 17:04:40 +0530

    initial commit

---
+++ Sorting/Print.java
@@ -0,0 +1,44 @@
+// public class Print {
+    //    start
+//     public static void main (String args[]){
+//         System.out.println("hello world");
+//     }
+// }
+
+// public class Print{
+//     public static void main (String args[]){
+        // output
+//         System.out.println("hey");
+//         System.out.println("Shubham This side");
+//         System.out.println("Shubham is best person");
+//         System.out.print("Shubham is bit person");
+        
+       
+
+//     }
+// }
+
+import java.util.Scanner;
+
+import java.util.*;
+public class Print{
+
+    // Calculations 
+    public static void main(String args[]){
+        // int a =20;
+        // int b =26;
+        // // int sum = a+b;
+        // int mul = a*b;
+        // System.out.println(mul);
+
+        Scanner sc = new Scanner(System.in);
+     
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        
+        System.out.println(sum);
+
+    }
+}
